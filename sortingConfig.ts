8da1ae2a9e37f09192b6f621a34ae851e1fb81ad
Harshal <gahlotharshal@gmail.com>
2025-12-03 13:25:00 +0530

    sorting config.ts

---
+++ sortingConfig.ts
@@ -0,0 +1,9 @@
+// WHICH EVER IS THE TARGET SORTING ALGO, IMPORT THAT AS * Module:
+
+// import * as Module from "./SelectionSortTUF";
+// import * as Module from "./BubbleSortTUF";
+import * as Module from "./InsertionSortTUF";
+
+export default Module;
+export type testCasesType1Type = [number[], number[]][];
+export type testCasesType2Type = number[][];
